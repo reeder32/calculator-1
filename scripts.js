76f440dc69c50d48da1b228a38fbb99b34189057
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-03-22 11:42:59 -0700

    add initial files
    Co-authored-by: Seth Medeiros <medeirosseth@gmail.com>

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function add(number1, number2) {
+	return number1 + number2;
+}
+
+const number1 = parseInt(prompt('enter a number:'));
+const number2 = parseInt(prompt('enter another number:'));
+
+alert(add(number1, number2));
+
+
