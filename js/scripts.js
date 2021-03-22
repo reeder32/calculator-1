328d7d01af44e60b32139fb70e6b04eed34e510d
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-03-22 13:13:47 -0700

    add javascript functions
    Co-authored-by: Seth Medeiros <medeirosseth@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,21 @@ function add(number1, number2) {
 const number1 = parseInt(prompt('enter a number:'));
 const number2 = parseInt(prompt('enter another number:'));
 
-alert(add(number1, number2));
+
+function divide(number1, number2) {
+	return number1 / number2;
+}
+
+function stubtract(number1, number2) {
+	return number1 - number2;
+}
+
+function multiply(number1, number2) {
+	return number1 * number2;
+}
+
+
+const result = multiply(number1, number2);
+alert(result);
 
 
