065676009fb37cd3c7a2e93937e3487655922ebc
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-03-25 16:56:45 -0700

    add edited files
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>
    Co-authored-by: Seth Medeiros <Medeirosseth@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,40 @@
 //business logic:
 function add(number1, number2) {
-	return number1 + number2;
+  return number1 + number2;
 }
 
-//const number1 = parseInt(prompt('enter a number:'));
-//const number2 = parseInt(prompt('enter another number:'));
-
-
 function divide(number1, number2) {
-	return number1 / number2;
+  return number1 / number2;
 }
 
 function subtract(number1, number2) {
-	return number1 - number2;
+  return number1 - number2;
 }
 
 function multiply(number1, number2) {
-	return number1 * number2;
+  return number1 * number2;
 }
 
-//const result = multiply(number1, number2);
-//alert(result);
-
 //userinterface logic:
-$(document).ready(function(){
-  $("#add-button").click(function(event){
-    const number1=parseInt($("#number1").val());
-    const number2=parseInt($("#number2").val());
-    const result=add(number1, number2);
-    $("#output").text(result);
+$(document).ready(function () {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
+    const operator = $("input:radio[name='operator']:checked").val();
+    console.log(operator);
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    console.log(result);
+    $("#output").text(result);
+    
   });
-  $("#subtract-button").click(function(event) {
-    const number1=parseInt($("#number1").val());
-    const number2=parseInt($("#number2").val());
-    const result=subtract(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-  $("#multiply-button").click(function(event) {
-    const number1=parseInt($("#number1").val());
-    const number2=parseInt($("#number2").val());
-    const result=multiply(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-  $("#divide-button").click(function(event) {
-    const number1=parseInt($("#number1").val());
-    const number2=parseInt($("#number2").val());
-    const result=divide(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  })
 });
-
-
